0e5ffea4e676922fa640bc003ededf676d5cc149
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-09-29 21:44:05 +0300

    register developing process 2

--- src/main/webapp/java/controller/auth/register/RegisterHandlerServlet.java
+++ src/main/webapp/java/controller/auth/register/RegisterHandlerServlet.java
@@ -1,16 +1,32 @@
 package controller.auth.register;
 
+import dao.UserDAO;
+import entity.User;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 public class RegisterHandlerServlet {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
         String userLogin = request.getParameter("login");
         String userPassword = request.getParameter("password");
+        User newUser = new User(0, userPassword);
 
-        
+        if (userDAO.checkLogin(userLogin) == false) {
+            userDAO.addUser(newUser, userPassword);
+        }
+        else {
+
+        }
 
     }
 
