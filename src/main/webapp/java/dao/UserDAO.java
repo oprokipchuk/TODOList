e04b4738959ae365636cc4a433ed1b4268d970d8
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-09-29 21:22:01 +0300

    add userDAO

--- src/main/webapp/java/dao/UserDAO.java
+++ src/main/webapp/java/dao/UserDAO.java
@@ -1,4 +1,41 @@
 package dao;
 
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
+
+    private static Connection connection;
+
+    public static void setConnection(Connection connection) {
+        UserDAO.connection = connection;
+    }
+
+    public boolean checkLogin(String login) throws SQLException {
+
+        String sql = "SELECT login FROM user WHERE login = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, login);
+        ResultSet resultOfQuery = statement.executeQuery();
+
+        boolean existsLogin = resultOfQuery.next();
+
+        statement.close();
+        return resultOfQuery.next();
+    }
+
+    public void addUser(User user, String password) throws SQLException {
+
+        String sql = "INSERT INTO user (login, password) VALUES (?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, user.getLogin());
+        statement.setString(2, password);
+        statement.executeUpdate();
+
+    }
+
 }
