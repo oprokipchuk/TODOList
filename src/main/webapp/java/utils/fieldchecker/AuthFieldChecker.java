f81fb21d78b6e6ed00ae6fdc50089a84760f2203
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-09-30 13:07:20 +0300

    add register field check

--- src/main/webapp/java/utils/fieldchecker/AuthFieldChecker.java
+++ src/main/webapp/java/utils/fieldchecker/AuthFieldChecker.java
@@ -1,5 +1,10 @@
 package utils.fieldchecker;
 
+import utils.fieldchecker.error.ErrorCode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AuthFieldChecker {
 
     public ErrorCodes checkLogin(String login) {
@@ -8,11 +13,19 @@ public class AuthFieldChecker {
             return ErrorCodes.INCORRECT_LOGIN_LENGTH;
         }
 
+        String regExp = "[A-Za-z0-9_]+";
+        Pattern correctLoginPattern = Pattern.compile(regExp);
+        Matcher matcher = correctLoginPattern.matcher(login);
+
+        if (matcher.matches() == false) {
+            return ErrorCodes.INCORRECT_LOGIN_PATTERN;
+        }
+
         return ErrorCodes.CHECK_PASSED;
     }
 
 
-    public enum ErrorCodes {
+    public enum ErrorCodes implements ErrorCode {
 
         CHECK_PASSED {
             @Override
@@ -25,9 +38,19 @@ public class AuthFieldChecker {
             public String getMessage() {
                 return "login must be longer than 4 and shorter than 20 characters!";
             }
+        },
+        INCORRECT_LOGIN_PATTERN{
+            @Override
+            public String getMessage() {
+                return "login can contain only A-Z, a-z, 0-9 and '_'";
+            }
         };
 
         public abstract String getMessage();
+
+        public boolean isError() {
+            return !(this == CHECK_PASSED);
+        }
 
     }
 }
